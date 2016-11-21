63443e407c003590b22e103eb14d069146834d32
“ricardbernal81” <ricardbernal81@gmail.com>
2016-11-21 13:19:43 +0100

    Incluimos permisos explícitos

--- app/src/main/java/com/example/ricardbernal/proveedorcontactosagendanomnum/MainActivity.java
+++ app/src/main/java/com/example/ricardbernal/proveedorcontactosagendanomnum/MainActivity.java
@@ -1,16 +1,22 @@
 package com.example.ricardbernal.proveedorcontactosagendanomnum;
 
 
+import android.Manifest;
 import android.content.ContentResolver;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.database.Cursor;
 import android.provider.ContactsContract.CommonDataKinds.Phone;
 import android.provider.ContactsContract.Data;
+import android.view.View;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 import android.support.v4.widget.CursorAdapter;
-
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -22,15 +28,55 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //lvContactos = (ListView)findViewById(lvContactos);
-        cargarContactos();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(this,
+                    Manifest.permission.READ_CONTACTS)
+                    != PackageManager.PERMISSION_GRANTED) {
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.READ_CONTACTS},
+                        100);
+                cargarContactos();
+
+            } else {
+                cargarContactos();
+            }
+        }
     }
+
+        //--------------
+
+
+
+        public void onRequestPermissionsResult (int requestCode,
+        String permissions[],
+        int[] grantResults){
+            switch (requestCode) {
+                case 100: {
+                    // If request is cancelled, the result arrays are empty.
+                    if (grantResults.length > 0
+                            && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                        cargarContactos();
+                    } else {
+                        Toast.makeText(this, "No se pueden mostrar los contactos", Toast.LENGTH_SHORT).show();
+                    }
+                    return;
+                }
+            }
+        }
+
+
+
+
+    //--------------
+
 
     public void cargarContactos(){
         ContentResolver resolver=this.getContentResolver();
 
         Cursor c=resolver.query(Data.CONTENT_URI,
                 null,
-                Data.MIMETYPE+"='"+Phone.CONTENT_ITEM_TYPE+"",
+                Data.MIMETYPE+"='"+Phone.CONTENT_ITEM_TYPE+"'",
                 null,
                 null);
 
